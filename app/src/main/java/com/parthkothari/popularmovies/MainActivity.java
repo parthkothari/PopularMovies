b083e838a2d5a8e2eac318b74c5de1a83735f20c
Parth Kothari <parth.kothari87@gmail.com>
2018-10-24 17:50:31 -0700

    refactor: Renamed sort_menu to menu
    Renaming the menu layout file to make it more generic.
    
    Future change - menu item to contain a refresh button to get movie data from network again

--- app/src/main/java/com/parthkothari/popularmovies/MainActivity.java
+++ app/src/main/java/com/parthkothari/popularmovies/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.sort_menu, menu);
+        inflater.inflate(R.menu.menu, menu);
 
         return true;
     }
